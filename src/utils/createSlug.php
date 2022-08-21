25a3d0dad12dbd679bee8ec1622af288976891f5
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-21 19:58:10 +0700

    membuat dynamic separator

--- src/utils/createSlug.php
+++ src/utils/createSlug.php
@@ -1,11 +1,11 @@
 <?php
 
-function createSlug(string $str): string
+function createSlug(string $str, string $separator = '-'): string
 {
   $slug = htmlspecialchars($str);
   $slug = trim($slug);
   $slug = strtolower($slug);
   $slug = explode(' ', $slug);
-  $slug = implode('-', $slug);
+  $slug = implode($separator, $slug);
   return $slug;
 }
